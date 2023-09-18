5e34a5b94f1bcf136320458af4e3abccbb8f8c0d
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2023-09-18 23:02:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
 import os
 import json
@@ -6,6 +7,8 @@ import tkinter
 import tkinter.messagebox
 import customtkinter
 import isUpdate as isUP
+from tksheet import Sheet
+import mysql.connector
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -37,42 +40,41 @@ class App(customtkinter.CTk):
 
         # new entry dilog box part
         self.addNew_frame = customtkinter.CTkFrame(self.tabview.tab("Kaval Master"))
-        self.addNew_frame.grid(row=0, column=0, padx=(20, 20), pady=(10, 10),sticky="nsew")
+        self.addNew_frame.grid(row=0, column=0, padx=(0, 5), pady=(10, 10),sticky="nsew")
+
+        self.head_label_inNew = customtkinter.CTkLabel(self.addNew_frame, text="Add Data in Database")
+        self.head_label_inNew.grid(row=0, column=0, columnspan=2, padx=(20, 20), pady=(10, 10))
 
         self.enterId_label_inNew = customtkinter.CTkLabel(self.addNew_frame, text="Enter ID")
-        self.enterId_label_inNew.grid(row=0, column=0, padx=(20, 20), pady=(20, 20))
+        self.enterId_label_inNew.grid(row=1, column=0, padx=(20, 20), pady=(5, 5))
         self.enter_id_inNew = customtkinter.CTkEntry(self.addNew_frame, placeholder_text="1909")
-        self.enter_id_inNew.grid(row=0, column=1, padx=(20, 20), pady=(20, 20))
+        self.enter_id_inNew.grid(row=1, column=1, padx=(20, 20), pady=(5, 5))
 
         self.enterName_label_inNew = customtkinter.CTkLabel(self.addNew_frame, text="Enter Name")
-        self.enterName_label_inNew.grid(row=1, column=0, padx=(20, 20), pady=(20, 20))
+        self.enterName_label_inNew.grid(row=2, column=0, padx=(20, 20), pady=(5, 5))
         self.enter_Name_inNew = customtkinter.CTkEntry(self.addNew_frame, placeholder_text="Kaval xx")
-        self.enter_Name_inNew.grid(row=1, column=1, padx=(20, 20), pady=(20, 20))
+        self.enter_Name_inNew.grid(row=2, column=1, padx=(20, 20), pady=(5, 10))
 
         self.enterNew_inNew = customtkinter.CTkButton(self.addNew_frame, text="Add into Database",
                                                            command=self.open_input_dialog_event)
-        self.enterNew_inNew.grid(row=2, column=0, columnspan=2, padx=(20, 20), pady=(20, 20))
+        self.enterNew_inNew.grid(row=3, column=0, columnspan=2, padx=(20, 20), pady=(10, 20))
 
-        # edit option dilog box
-        self.editDB_frame = customtkinter.CTkFrame(self.tabview.tab("Kaval Master"))
-        self.editDB_frame.grid(row=0, column=1, padx=(20, 20), pady=(10, 10),sticky="nsew")
+        # find option dilog box
+        self.findDB_frame = customtkinter.CTkFrame(self.tabview.tab("Kaval Master"))
+        self.findDB_frame.grid(row=0, column=1, padx=(5, 0), pady=(10, 10),sticky="nsew")
 
-        self.enterId_label_inEdit = customtkinter.CTkLabel(self.editDB_frame, text="Enter ID")
-        self.enterId_label_inEdit.grid(row=0, column=0, padx=(20, 20), pady=(20, 20))
-        self.enter_id_inEdit = customtkinter.CTkEntry(self.editDB_frame, placeholder_text="1909")
-        self.enter_id_inEdit.grid(row=0, column=1, padx=(20, 20), pady=(20, 20))
+        self.head_label_inFind = customtkinter.CTkLabel(self.findDB_frame, text="Scearch in Database")
+        self.head_label_inFind.grid(row=0, column=0, columnspan=2, padx=(20, 20), pady=(10, 10))
+        self.findBy_inEdit_Label = customtkinter.CTkLabel(self.findDB_frame, text="Find By", anchor="w")
+        self.findBy_inEdit_Label.grid(row=1, column=0, padx=20, pady=(5, 5))
+        self.findBy_inEdit_optionemenu = customtkinter.CTkOptionMenu(self.findDB_frame, values=["Kaval ID", "Kaval Name", "SNo."])
+        self.findBy_inEdit_optionemenu.grid(row=1, column=1, padx=20, pady=(5, 5))
+        self.data_label_inFind = customtkinter.CTkLabel(self.findDB_frame, text="Enter Respective Data")
+        self.data_label_inFind.grid(row=2, column=0, padx=(20, 20), pady=(5, 5))
+        self.enter_data_inFind = customtkinter.CTkEntry(self.findDB_frame, placeholder_text="enter data")
+        self.enter_data_inFind.grid(row=2, column=1, padx=(20, 20), pady=(5, 5))
 
-        self.enter_newName_label_inEdit = customtkinter.CTkLabel(self.editDB_frame, text="Enter New Name")
-        self.enter_newName_label_inEdit.grid(row=1, column=0, padx=(20, 20), pady=(20, 20))
-        self.enter_newName_inEdit = customtkinter.CTkEntry(self.editDB_frame, placeholder_text="Suku...")
-        self.enter_newName_inEdit.grid(row=1, column=1, padx=(20, 20), pady=(20, 20))
-
-        self.enter_newNameCof_label_inEdit = customtkinter.CTkLabel(self.editDB_frame, text="Conform New Name")
-        self.enter_newNameCof_label_inEdit.grid(row=2, column=0, padx=(20, 20), pady=(20, 20))
-        self.enter_newNameCof_inEdit = customtkinter.CTkEntry(self.editDB_frame, placeholder_text="Suku...")
-        self.enter_newNameCof_inEdit.grid(row=2, column=1, padx=(20, 20), pady=(20, 20))
-
-        self.editBtn_inEdit = customtkinter.CTkButton(self.editDB_frame, text="Save the changes",
+        self.editBtn_inEdit = customtkinter.CTkButton(self.findDB_frame, text="Find",
                                                            command=self.open_input_dialog_event)
         self.editBtn_inEdit.grid(row=3, column=0, columnspan=2, padx=(20, 20), pady=(20, 20))
 
@@ -80,15 +82,60 @@ class App(customtkinter.CTk):
         self.labelkm_frame = customtkinter.CTkFrame(self.tabview.tab("Kaval Master"), fg_color="transparent")
         self.labelkm_frame.grid(row=2, column=0, padx=(0, 0), pady=(10, 10), sticky="nsew")
         self.label_inKMlabel = customtkinter.CTkLabel(self.labelkm_frame, text="Database View Point")
-        self.label_inKMlabel.grid(row=0, column=0, columnspan=2, padx=(5, 5), pady=(5, 5),sticky="nsew")
+        self.label_inKMlabel.grid(row=0, column=0, padx=(5, 15), pady=(5, 5),sticky="nsew")
+
+        self.editBtn_inKMLabel = customtkinter.CTkButton(self.labelkm_frame, text="Edit",
+                                                           command=self.open_input_dialog_event)
+        self.editBtn_inKMLabel.grid(row=0, column=1, padx=(5, 5), pady=(5, 5),sticky="nsew")
+        self.deleteBtn_inKMLabel = customtkinter.CTkButton(self.labelkm_frame, text="Delete",
+                                                           command=self.delete_item_from_Kaval_Master)
+        self.deleteBtn_inKMLabel.grid(row=0, column=2, padx=(5, 5), pady=(5, 5),sticky="nsew")
 
         # view Tabel
-        self.viewTabel_km_frame = customtkinter.CTkFrame(self.tabview.tab("Kaval Master"))
+        self.viewTabel_km_frame = customtkinter.CTkScrollableFrame(self.tabview.tab("Kaval Master"))
         self.viewTabel_km_frame.grid(row=3, column=0, columnspan=2, padx=(5, 5), pady=(5, 5), sticky="nsew")
 
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir,"manifest.json")
+        json_data = read_json_file(file_path)
+        db = mysql.connector.connect(
+            host=json_data["Database"]["host"],
+            user=json_data["Database"]["username"],
+            password=json_data["Database"]["password"],
+            database=json_data["Database"]["database_name"]
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM kaval_master")
+        data = cursor.fetchall()
+        tree = ttk.Treeview(self.viewTabel_km_frame, columns=("SNo.", "Kaval ID", "Kaval Name"))
+
+        # Define column names
+        tree.heading("#1", text="SNo.")
+        tree.heading("#2", text="Kaval ID")
+        tree.heading("#3", text="Kaval Name")
+
+        # Insert data into the TreeView
+        for row in data:
+            tree.insert("", "end", values=row)
+
+        # Apply custom styling to the TreeView
+        tree.configure(style="Custom.Treeview")
+
+        # Define a custom style using tkinter's Style class
+        style = ttk.Style()
+        style.configure("Custom.Treeview")#, background="#f7f7f7")
+        style.map("Custom.Treeview")#, background=[("selected", "#347083")])
+
+        # Pack the TreeView
+        tree.pack(fill="both", expand=True)
+
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
 
         self.label_tab_2 = customtkinter.CTkLabel(self.tabview.tab("Tab 2"), text="Need to write code still")
         self.label_tab_2.grid(row=0, column=0, padx=20, pady=20)
+
         self.appearance_mode_label = customtkinter.CTkLabel(self.tabview.tab("Settings"), text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.tabview.tab("Settings"), values=["Light", "Dark", "System"],
@@ -100,10 +147,33 @@ class App(customtkinter.CTk):
                                                                command=self.change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
+    def refresh_table(self):
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir,"manifest.json")
+        json_data = read_json_file(file_path)
+        db = mysql.connector.connect(
+            host=json_data["Database"]["host"],
+            user=json_data["Database"]["username"],
+            password=json_data["Database"]["password"],
+            database=json_data["Database"]["database_name"]
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM kaval_master")
+        data = cursor.fetchall()
+        for item in self.tree.get_children():
+            self.tree.delete(item)
+        for row in data:
+            self.tree.insert("", "end", values=row)
+        cursor.close()
+        db.close()
 
     def open_input_dialog_event(self):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
         print("CTkInputDialog:", dialog.get_input())
+
+    def delete_item_from_Kaval_Master(self):
+        dialog = customtkinter.CTkInputDialog(text="Enter ID which you want to delete", title="Deleting Item")
+        print("ID need to Delete is :", dialog.get_input())
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -126,6 +196,7 @@ def notification(head,body):
 def open_main_window():
     if(isUP.isAvailabe()):
         isUP.updateNotify()
+    # Create a MySQL connection
     splash_root.destroy()
     app = App()
     app.mainloop()
