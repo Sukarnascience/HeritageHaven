dbdb679757203642383c90781aafc3102a515cee
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2023-09-19 00:19:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ class App(customtkinter.CTk):
         self.enter_Name_inNew.grid(row=2, column=1, padx=(20, 20), pady=(5, 10))
 
         self.enterNew_inNew = customtkinter.CTkButton(self.addNew_frame, text="Add into Database",
-                                                           command=self.open_input_dialog_event)
+                                                           command=self.add_to_the_kaval_master)
         self.enterNew_inNew.grid(row=3, column=0, columnspan=2, padx=(20, 20), pady=(10, 20))
 
         # find option dilog box
@@ -75,7 +75,7 @@ class App(customtkinter.CTk):
         self.enter_data_inFind.grid(row=2, column=1, padx=(20, 20), pady=(5, 5))
 
         self.editBtn_inEdit = customtkinter.CTkButton(self.findDB_frame, text="Find",
-                                                           command=self.open_input_dialog_event)
+                                                           command=self.findData_from_kaval_master)
         self.editBtn_inEdit.grid(row=3, column=0, columnspan=2, padx=(20, 20), pady=(20, 20))
 
         # Just Label to view Tabel
@@ -85,7 +85,7 @@ class App(customtkinter.CTk):
         self.label_inKMlabel.grid(row=0, column=0, padx=(5, 15), pady=(5, 5),sticky="nsew")
 
         self.editBtn_inKMLabel = customtkinter.CTkButton(self.labelkm_frame, text="Edit",
-                                                           command=self.open_input_dialog_event)
+                                                           command=self.edit_item_from_Kaval_Master)
         self.editBtn_inKMLabel.grid(row=0, column=1, padx=(5, 5), pady=(5, 5),sticky="nsew")
         self.deleteBtn_inKMLabel = customtkinter.CTkButton(self.labelkm_frame, text="Delete",
                                                            command=self.delete_item_from_Kaval_Master)
@@ -107,19 +107,19 @@ class App(customtkinter.CTk):
         cursor = db.cursor()
         cursor.execute("SELECT * FROM kaval_master")
         data = cursor.fetchall()
-        tree = ttk.Treeview(self.viewTabel_km_frame, columns=("SNo.", "Kaval ID", "Kaval Name"))
+        self.tree = ttk.Treeview(self.viewTabel_km_frame, columns=("SNo.", "Kaval ID", "Kaval Name"))
 
         # Define column names
-        tree.heading("#1", text="SNo.")
-        tree.heading("#2", text="Kaval ID")
-        tree.heading("#3", text="Kaval Name")
+        self.tree.heading("#1", text="SNo.")
+        self.tree.heading("#2", text="Kaval ID")
+        self.tree.heading("#3", text="Kaval Name")
 
         # Insert data into the TreeView
         for row in data:
-            tree.insert("", "end", values=row)
+            self.tree.insert("", "end", values=row)
 
         # Apply custom styling to the TreeView
-        tree.configure(style="Custom.Treeview")
+        self.tree.configure(style="Custom.Treeview")
 
         # Define a custom style using tkinter's Style class
         style = ttk.Style()
@@ -127,7 +127,7 @@ class App(customtkinter.CTk):
         style.map("Custom.Treeview")#, background=[("selected", "#347083")])
 
         # Pack the TreeView
-        tree.pack(fill="both", expand=True)
+        self.tree.pack(fill="both", expand=True)
 
         # Close the cursor and database connection
         cursor.close()
@@ -148,6 +148,159 @@ class App(customtkinter.CTk):
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
     def refresh_table(self):
+        # Read JSON data from the manifest.json file
+        try:
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            file_path = os.path.join(script_dir, "manifest.json")
+
+            with open(file_path, "r") as json_file:
+                json_data = json.load(json_file)
+
+            db = mysql.connector.connect(
+                host=json_data["Database"]["host"],
+                user=json_data["Database"]["username"],
+                password=json_data["Database"]["password"],
+                database=json_data["Database"]["database_name"]
+            )
+
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM kaval_master")
+            data = cursor.fetchall()
+
+            for item in self.tree.get_children():
+                self.tree.delete(item)
+
+            for row in data:
+                self.tree.insert("", "end", values=row)
+
+            cursor.close()
+            db.close()
+
+        except Exception as e:
+            # Handle exceptions here (e.g., file not found, database connection error)
+            print(f"An error occurred: {str(e)}")
+
+    def add_to_the_kaval_master(self):
+        # Retrieve user-entered ID and Name from the Entry widgets
+        new_id = self.enter_id_inNew.get()
+        new_name = self.enter_Name_inNew.get()
+
+        # Validate that both ID and Name are provided
+        if not new_id or not new_name:
+            # Show an error message or handle the validation as needed
+            print("Both ID and Name are required.")
+            return
+
+        try:
+            # Create a MySQL connection
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            file_path = os.path.join(script_dir,"manifest.json")
+            json_data = read_json_file(file_path)
+            db = mysql.connector.connect(
+                host=json_data["Database"]["host"],
+                user=json_data["Database"]["username"],
+                password=json_data["Database"]["password"],
+                database=json_data["Database"]["database_name"]
+            )
+
+            # Create a cursor
+            cursor = db.cursor()
+
+            # Insert the new data into the MySQL table
+            insert_query = "INSERT INTO kaval_master (Kaval_ID, Kaval_Name) VALUES (%s, %s)"
+            cursor.execute(insert_query, (new_id, new_name))
+
+            # Commit the changes to the database
+            db.commit()
+
+            # Close the cursor and database connection
+            cursor.close()
+            db.close()
+
+            # Clear the Entry widgets after adding the data
+            self.enter_id_inNew.delete(0, "end")
+            self.enter_Name_inNew.delete(0, "end")
+
+            # Optionally, refresh the table to reflect the new data
+            self.refresh_table()
+
+        except Exception as e:
+            # Handle exceptions here (e.g., database connection error)
+            print(f"An error occurred while adding data: {str(e)}")
+
+
+    def findData_from_kaval_master(self):
+        # Retrieve user-selected search criteria and entered data
+        find_by = self.findBy_inEdit_optionemenu.get()
+        entered_data = self.enter_data_inFind.get()
+
+        # Validate that the entered data is not empty
+        if not entered_data:
+            # Show an error message or handle the validation as needed
+            print("Please enter data to search.")
+            return
+
+        try:
+            # Create a MySQL connection
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            file_path = os.path.join(script_dir,"manifest.json")
+            json_data = read_json_file(file_path)
+            db = mysql.connector.connect(
+                host=json_data["Database"]["host"],
+                user=json_data["Database"]["username"],
+                password=json_data["Database"]["password"],
+                database=json_data["Database"]["database_name"]
+            )
+
+            # Create a cursor
+            cursor = db.cursor()
+
+            # Define the query based on the selected search criteria
+            if find_by == "Kaval ID":
+                query = "SELECT * FROM kaval_master WHERE Kaval_ID = %s"
+            elif find_by == "Kaval Name":
+                query = "SELECT * FROM kaval_master WHERE Kaval_Name = %s"
+            elif find_by == "SNo.":
+                query = "SELECT * FROM kaval_master WHERE SNo = %s"
+            else:
+                # Handle unsupported search criteria
+                print("Unsupported search criteria.")
+                return
+
+            # Execute the query with the entered data
+            cursor.execute(query, (entered_data,))
+            data = cursor.fetchall()
+
+            # Clear the TreeView to display the search results
+            for item in self.tree.get_children():
+                self.tree.delete(item)
+
+            # Insert the search results into the TreeView
+            for row in data:
+                self.tree.insert("", "end", values=row)
+
+            # Close the cursor and database connection
+            cursor.close()
+            db.close()
+
+        except Exception as e:
+            # Handle exceptions here (e.g., database connection error)
+            print(f"An error occurred while searching: {str(e)}")
+
+    def open_input_dialog_event(self):
+        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
+        print("CTkInputDialog:", dialog.get_input())
+
+    def delete_item_from_Kaval_Master(self):
+        # Create an input dialog to get the ID to delete
+        dialog = customtkinter.CTkInputDialog(text="Enter ID which you want to delete", title="Deleting Item")
+        selected_id = dialog.get_input()
+
+        if not selected_id:
+            # No ID entered
+            return
+
+        # Create a MySQL connection
         script_dir = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(script_dir,"manifest.json")
         json_data = read_json_file(file_path)
@@ -157,23 +310,62 @@ class App(customtkinter.CTk):
             password=json_data["Database"]["password"],
             database=json_data["Database"]["database_name"]
         )
+
+        # Create a cursor
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM kaval_master")
-        data = cursor.fetchall()
-        for item in self.tree.get_children():
-            self.tree.delete(item)
-        for row in data:
-            self.tree.insert("", "end", values=row)
+
+        # Delete the data from the MySQL table based on the entered ID
+        cursor.execute("DELETE FROM kaval_master WHERE Kaval_ID = %s", (selected_id,))
+
+        # Commit the changes to the database
+        db.commit()
+
+        # Close the cursor and database connection
         cursor.close()
         db.close()
 
-    def open_input_dialog_event(self):
-        dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
-        print("CTkInputDialog:", dialog.get_input())
+        # Refresh the table to reflect the changes
+        self.refresh_table()
 
-    def delete_item_from_Kaval_Master(self):
-        dialog = customtkinter.CTkInputDialog(text="Enter ID which you want to delete", title="Deleting Item")
-        print("ID need to Delete is :", dialog.get_input())
+    def edit_item_from_Kaval_Master(self):
+        # Create an input dialog to get the ID to edit
+        dialog = customtkinter.CTkInputDialog(text="Enter ID to edit", title="Editing Item")
+        selected_id = dialog.get_input()
+
+        if not selected_id:
+            # No ID entered
+            return
+
+        # Create an input dialog to get the new name
+        new_name_dialog = customtkinter.CTkInputDialog(text="Enter the new name", title="Editing Name")
+        new_name = new_name_dialog.get_input()
+
+        if not new_name:
+            # No new name entered
+            return
+
+        # Create a MySQL connection
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(script_dir,"manifest.json")
+        json_data = read_json_file(file_path)
+        db = mysql.connector.connect(
+            host=json_data["Database"]["host"],
+            user=json_data["Database"]["username"],
+            password=json_data["Database"]["password"],
+            database=json_data["Database"]["database_name"]
+        )
+
+        # Create a cursor
+        cursor = db.cursor()
+        # Update the name in the MySQL table based on the selected ID
+        cursor.execute("UPDATE kaval_master SET Kaval_Name = %s WHERE Kaval_ID = %s", (new_name, selected_id))
+        # Commit the changes to the database
+        db.commit()
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+        # Refresh the table to reflect the changes
+        self.refresh_table()
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -194,8 +386,8 @@ def notification(head,body):
     pass
 
 def open_main_window():
-    if(isUP.isAvailabe()):
-        isUP.updateNotify()
+    #if(isUP.isAvailabe()):
+    #    isUP.updateNotify()
     # Create a MySQL connection
     splash_root.destroy()
     app = App()
