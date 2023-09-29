846d255354eb5fee8f33fb427869a3a0f6726839
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2023-09-29 19:23:42 +0530

    Update drop_db_tabel_dummy.sql

--- database/drop_db_tabel_dummy.sql
+++ database/drop_db_tabel_dummy.sql
@@ -1,6 +1,6 @@
 -- @Author      : Sukarna Jana
 -- @Version     : 0.0.1V
--- @Last Update : 18-09-2023
+-- @Last Update : 29-09-2023
 -- @Brief       : To Drop dummy data / or full setup in database
 -- @SQL Version : 8.0.34
 -- @Note        : you need to Uncomments the line which you want to bring into action [ by removing "--" from front]
@@ -13,6 +13,7 @@ DELETE FROM kaval_master;
 
 -- Drop Table only
 -- DROP TABLE kaval_master;
+-- DROP TABLE surname_master;
 
 -- Drop Database only
 -- DROP DATABASE heritagehaven;
