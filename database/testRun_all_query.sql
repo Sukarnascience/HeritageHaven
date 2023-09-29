9c5b5307c01f8a3c661552cafcf972d9c06bb553
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2023-09-29 19:25:56 +0530

    Update testRun_all_query.sql

--- database/testRun_all_query.sql
+++ database/testRun_all_query.sql
@@ -1,6 +1,6 @@
 -- @Author      : Sukarna Jana
 -- @Version     : 0.0.1V
--- @Last Update : 18-09-2023
+-- @Last Update : 29-09-2023
 -- @Brief       : Create DataBase and Tables by which we can use for setuping everything for Software to run
 
 -- Creating Database
@@ -17,9 +17,16 @@ CREATE TABLE kaval_master (
     UNIQUE(Kaval_ID),
     CHECK(Kaval_ID >= 0 AND Kaval_ID <= 9999)
 );
+CREATE TABLE surname_master (
+    Surname_ID INT UNSIGNED NOT NULL,
+    Surname_Name VARCHAR(40) NOT NULL,
+    UNIQUE(Surname_ID),
+    CHECK(Surname_ID >= 0 AND Surname_ID <= 9999)
+);
 
 -- Debug 
 SELECT * FROM kaval_master;
+SELECT * FROM surname_master;
 
 INSERT INTO kaval_master (Kaval_ID,Kaval_Name)
 VALUES
@@ -29,5 +36,14 @@ VALUES
 (1001, "UNKNOWN"),
 (1002, "UNKNOWN");
 
+INSERT INTO surname_master(Surname_ID,Surname_Name)
+VALUES
+(1909, "Jana"),
+(1109, "Athikary"),
+(1000, "UNKNOWN"),
+(1001, "UNKNOWN"),
+(1002, "UNKNOWN");
+
 -- Debug 
 SELECT * FROM kaval_master;
+SELECT * FROM surname_master;
